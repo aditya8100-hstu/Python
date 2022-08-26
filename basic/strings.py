4f86097d27b6b7b4ecee5889df0dc0ef0747ed31
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-08-26 17:56:39 +0600

    strings

---
+++ basic/strings.py
@@ -0,0 +1,8 @@
+a = input("Enter a string : ")
+
+print("The length of string : ", len(a))
+print("Make only the first letter capital: ", a.capitalize())
+print("Make the full string capital: ", a.upper())
+print("Find the first five letters: ", a[0:5])
+print("Split the full string : ", a.split())
+#help(str)
