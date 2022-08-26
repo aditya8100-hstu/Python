a40f276d1a1d2d97386be5fe3720641cd06b5d4d
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-08-26 17:55:37 +0600

    arithmetic operators

---
+++ basic/arithmatic operators.py
@@ -0,0 +1,14 @@
+a = float(input("Enter a number: "))
+b = float(input("Enter a number: "))
+
+x = a + b  # add two numbers
+y = a - b  # subtract two numbers
+z = a * b  # multiply two numbers
+w = a / b  # division of two numbers
+p = a % b  # mod of a number
+
+print("The result is: ", x)
+print("The result is: ", y)
+print("The result is: ", z)
+print("The result is: ", w)
+print("The result is: ", p)
