41eef30620df48956b4a4793ea5ad26a5e20ff5c
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-08-26 17:56:19 +0600

    list

---
+++ basic/list.py
@@ -0,0 +1,7 @@
+list = [1, [20, "adi", 40, 50.3], 20.3, 10, 40, 3]
+
+list = list + [2, 4, 5, 7, 8, 2.6]  # add two list with one another
+
+list.remove(list[2])      # to remove a particular element from the list using its index number
+
+print(list)
