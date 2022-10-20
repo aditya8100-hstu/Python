de532f41348287bcc2920d86e9c2b00618f063f0
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-10-20 19:02:32 +0600

    Add files via upload

---
+++ basic/letterGrade.py
@@ -0,0 +1,9 @@
+number = int(input("Enter your number: "))
+if 80 <= number <= 100:
+    print("A+")
+elif 70 <= number <= 79:
+    print("A-")
+elif 60 <= number <= 69:
+    print("A")
+else:
+    print("Fail")
