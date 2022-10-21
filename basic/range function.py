d34bc2a7464b3ae77438034c7daf15f78a2353cb
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-10-21 17:47:32 +0600

    Range function basic

---
+++ basic/range function.py
@@ -0,0 +1,11 @@
+num = range(10)  # This means a range from 0 to 9
+print(num)  # Output --> range(0, 10)
+
+num1 = list(range(10))  # This means a range from 0 to 9 which is stored in a list
+print(num1)  # Output --> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+num2 = list(range(5, 20))  # This means a range from 5 to 19 which is stored in a list
+print(num2)  # Output --> [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+
+num3 = list(range(5, 20, 2))  # This means a range from 5 to 19 with 2 distance which is stored in a list
+print(num3)  # Output --> [5, 7, 9, 11, 13, 15, 17, 19]
