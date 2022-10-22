82dd43dde8bca408d383046187ec3e94ecd86626
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-10-22 23:41:48 +0600

    Calculate the multiplication and sum of two numbers

---
+++ basic/PYnative/Basic Exercise for Beginners/Exercise 1.py
@@ -0,0 +1,10 @@
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+
+multiplicationOfTwoNumber = number1 * number2
+
+if multiplicationOfTwoNumber <= 1000:
+    print("The result is", multiplicationOfTwoNumber)
+else:
+    sum = number1 + number2
+    print("The result is", sum)
